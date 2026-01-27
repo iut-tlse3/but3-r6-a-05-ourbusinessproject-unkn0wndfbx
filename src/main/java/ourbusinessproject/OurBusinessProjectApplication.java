2969a2afb5ddc65acab70a1a69e08a0551a89106
THDev <titouan.helbert@gmail.com>
2026-01-27 09:18:26 +0100

    first commit

---
+++ src/main/java/ourbusinessproject/OurBusinessProjectApplication.java
@@ -0,0 +1,13 @@
+package ourbusinessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBusinessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurBusinessProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ourbusinessproject/OurBusinessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ourbusinessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBusinessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
